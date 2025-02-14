a01ba4032fc9992d9b73dbeb165dd11b90e54d9a
marco-vanin <marco.vanin.pro@gmail.com>
2025-02-14 18:35:10 +0100

    Borrow Functionnality

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,12 +1,15 @@
+import React from "react";
 import Image from "next/image";
-import { Button } from "./ui/button";
-import BookCover from "./BookCover";
+import BookCover from "@/components/BookCover";
+import BorrowBook from "@/components/BorrowBook";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 interface Props extends Book {
   userId: string;
 }
-
-const BookOverview = ({
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -16,7 +19,22 @@ const BookOverview = ({
   description,
   coverColor,
   coverUrl,
+  id,
+  userId,
 }: Props) => {
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, userId))
+    .limit(1);
+
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
+    message:
+      availableCopies <= 0
+        ? "Book is not available"
+        : "You are not eligible to borrow this book",
+  };
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
@@ -36,24 +54,27 @@ const BookOverview = ({
             <Image src="/icons/star.svg" alt="star" width={22} height={22} />
             <p>{rating}</p>
           </div>
-
-          <div className="book-copies">
-            <p>
-              Total Books : <span>{totalCopies}</span>
-            </p>
-
-            <p>
-              Availabe Books : <span>{availableCopies}</span>
-            </p>
-          </div>
-
-          <p className="book-description">{description}</p>
-
-          <Button className="book-overview_btn">
-            <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-            <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
-          </Button>
         </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books <span>{totalCopies}</span>
+          </p>
+
+          <p>
+            Available Books <span>{availableCopies}</span>
+          </p>
+        </div>
+
+        <p className="book-description">{description}</p>
+
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
 
       <div className="relative flex flex-1 justify-center">
@@ -77,4 +98,5 @@ const BookOverview = ({
     </section>
   );
 };
+
 export default BookOverview;
